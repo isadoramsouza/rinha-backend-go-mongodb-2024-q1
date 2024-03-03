3c8cae73b472b1f2d552f6318c6cf8406444243b
Isadora Souza <isadora_msouza@hotmail.com>
2024-03-03 15:33:59 -0300

    Revert "adjust goroutines"
    This reverts commit 75cf48070ca5758ba00b76a0951013f88252b520.

--- go.mod
+++ go.mod
@@ -25,7 +25,6 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -33,7 +32,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -47,6 +45,5 @@ require (
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/transacao/service.go
+++ internal/transacao/service.go
@@ -13,72 +13,70 @@ type Service interface {
 
 type transacaoService struct {
 	repository Repository
-	semaphore  chan struct{}
 }
 
 func NewService(r Repository) Service {
 	return &transacaoService{
 		repository: r,
-		semaphore:  make(chan struct{}, 20),
 	}
 }
 
+var semaphore = make(chan struct{}, 20)
+
 func (s *transacaoService) CreateTransaction(ctx context.Context, t domain.Transacao) (domain.TransacaoResponse, error) {
-	responseChan := make(chan domain.TransacaoResponse)
-	errChan := make(chan error)
+	responseChan := make(chan domain.TransacaoResponse, 1)
+	errChan := make(chan error, 1)
 
-	select {
-	case s.semaphore <- struct{}{}:
+	semaphore <- struct{}{}
+
+	go func() {
 		defer func() {
-			<-s.semaphore
+			<-semaphore
 		}()
 
-		go func() {
-			response, err := s.repository.SaveTransaction(ctx, t)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			responseChan <- response
-		}()
-	case <-ctx.Done():
-		return domain.TransacaoResponse{}, ctx.Err()
-	}
+		response, err := s.repository.SaveTransaction(ctx, t)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		responseChan <- response
+	}()
 
 	select {
 	case response := <-responseChan:
 		return response, nil
 	case err := <-errChan:
 		return domain.TransacaoResponse{}, err
+	case <-ctx.Done():
+		return domain.TransacaoResponse{}, ctx.Err()
 	}
 }
 
 func (s *transacaoService) GetExtrato(ctx context.Context, id int) (domain.Extrato, error) {
-	extratoChan := make(chan domain.Extrato)
-	errChan := make(chan error)
+	extratoChan := make(chan domain.Extrato, 1)
+	errChan := make(chan error, 1)
 
-	select {
-	case s.semaphore <- struct{}{}:
+	semaphore <- struct{}{}
+
+	go func() {
 		defer func() {
-			<-s.semaphore
+			<-semaphore
 		}()
 
-		go func() {
-			extrato, err := s.repository.GetExtrato(ctx, id)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			extratoChan <- extrato
-		}()
-	case <-ctx.Done():
-		return domain.Extrato{}, ctx.Err()
-	}
+		extrato, err := s.repository.GetExtrato(ctx, id)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		extratoChan <- extrato
+	}()
 
 	select {
 	case extrato := <-extratoChan:
 		return extrato, nil
 	case err := <-errChan:
 		return domain.Extrato{}, err
+	case <-ctx.Done():
+		return domain.Extrato{}, ctx.Err()
 	}
 }
