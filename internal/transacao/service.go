75cf48070ca5758ba00b76a0951013f88252b520
Isadora Souza <isadora_msouza@hotmail.com>
2024-03-03 15:25:55 -0300

    adjust goroutines

--- internal/transacao/service.go
+++ internal/transacao/service.go
@@ -13,70 +13,72 @@ type Service interface {
 
 type transacaoService struct {
 	repository Repository
+	semaphore  chan struct{}
 }
 
 func NewService(r Repository) Service {
 	return &transacaoService{
 		repository: r,
+		semaphore:  make(chan struct{}, 20),
 	}
 }
 
-var semaphore = make(chan struct{}, 20)
-
 func (s *transacaoService) CreateTransaction(ctx context.Context, t domain.Transacao) (domain.TransacaoResponse, error) {
-	responseChan := make(chan domain.TransacaoResponse, 1)
-	errChan := make(chan error, 1)
+	responseChan := make(chan domain.TransacaoResponse)
+	errChan := make(chan error)
 
-	semaphore <- struct{}{}
-
-	go func() {
+	select {
+	case s.semaphore <- struct{}{}:
 		defer func() {
-			<-semaphore
+			<-s.semaphore
 		}()
 
-		response, err := s.repository.SaveTransaction(ctx, t)
-		if err != nil {
-			errChan <- err
-			return
-		}
-		responseChan <- response
-	}()
+		go func() {
+			response, err := s.repository.SaveTransaction(ctx, t)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			responseChan <- response
+		}()
+	case <-ctx.Done():
+		return domain.TransacaoResponse{}, ctx.Err()
+	}
 
 	select {
 	case response := <-responseChan:
 		return response, nil
 	case err := <-errChan:
 		return domain.TransacaoResponse{}, err
-	case <-ctx.Done():
-		return domain.TransacaoResponse{}, ctx.Err()
 	}
 }
 
 func (s *transacaoService) GetExtrato(ctx context.Context, id int) (domain.Extrato, error) {
-	extratoChan := make(chan domain.Extrato, 1)
-	errChan := make(chan error, 1)
+	extratoChan := make(chan domain.Extrato)
+	errChan := make(chan error)
 
-	semaphore <- struct{}{}
-
-	go func() {
+	select {
+	case s.semaphore <- struct{}{}:
 		defer func() {
-			<-semaphore
+			<-s.semaphore
 		}()
 
-		extrato, err := s.repository.GetExtrato(ctx, id)
-		if err != nil {
-			errChan <- err
-			return
-		}
-		extratoChan <- extrato
-	}()
+		go func() {
+			extrato, err := s.repository.GetExtrato(ctx, id)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			extratoChan <- extrato
+		}()
+	case <-ctx.Done():
+		return domain.Extrato{}, ctx.Err()
+	}
 
 	select {
 	case extrato := <-extratoChan:
 		return extrato, nil
 	case err := <-errChan:
 		return domain.Extrato{}, err
-	case <-ctx.Done():
-		return domain.Extrato{}, ctx.Err()
 	}
 }
